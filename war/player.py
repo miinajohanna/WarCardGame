b6de1eca6ec1f1ec4eece9b572c5cb515c8e79c4
Courage Rasanen <Courage.rasanen0004@stud.hkr.se>
2023-03-09 22:58:17 +0100

    flake8 for player.py

--- war/player.py
+++ war/player.py
@@ -1,13 +1,18 @@
 class Player:
-    def __init__(self, name="Computer", score=0):
+    """The class of a player."""
+
+    def __init__(self, name="Computer"):
+        """Name of player."""
         self.name = name
-        self.score = score
 
     def changeName(self, newName):
+        """Set new name."""
         self.name = newName
 
     def displayName(self):
+        """Get name."""
         return self.name
 
     def __repr__(self):
+        """Return string."""
         return self.name
