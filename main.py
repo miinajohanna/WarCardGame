169243dc6f2d1a558243099d24344a9dee860d2f
Miina <125378412+miinajohanna@users.noreply.github.com>
2023-03-10 14:13:16 +0100

    cmd class implementation

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+
+-------- Welcome to a game of W A R! --------.
+
+Highest cards win a round and the first one to win
+ all cards wins the game.
+
+WAR can change the whole game.
+
+"""
+
+import shell
+
+
+if __name__ == "__main__":
+    print(__doc__)
+    shell.Shell().cmdloop()
