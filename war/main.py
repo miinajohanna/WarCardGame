b990d2cdfa25e904f8de0e835db71517bf2bc010
Miina <125378412+miinajohanna@users.noreply.github.com>
2023-03-10 15:14:10 +0100

    Delete main.py

--- war/main.py
+++
@@ -1,9 +0,0 @@
-"""-------- Welcome to a game of W A R! --------."""
-
-import game
-
-
-if __name__ == "__main__":
-    print(__doc__)
-    game_environment = game.Game()
-    game_environment.game()
